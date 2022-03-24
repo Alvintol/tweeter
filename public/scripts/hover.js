3872fa9da36705db767e5df9354ff75210f05ad6
Alvintol <alvintol+github@hotmail.com>
2022-03-24 03:28:54 +0000

    corrects tweet and icon hover animations

---
+++ public/scripts/hover.js
@@ -0,0 +1,13 @@
+$(() => {
+  let tweets = document.querySelectorAll('.tweet')
+  tweets.forEach(tweet => {
+    tweet.addEventListener('mouseover', function() {
+    this.style.boxShadow = '2px 2px 2px 2px white';
+    this.style.border    = 'solid thick';
+  })
+    tweet.addEventListener('mouseout', function() {
+    this.style.boxShadow = '';
+    this.style.border    = '';
+  })
+  })
+});
--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,7 +1,7 @@
 $(() => {
 
-  document.querySelector('#new-tweet-link').addEventListener('click', () => {
-    alert('You clicked to write a new tweet!');
+  document.querySelector('.nav-tweet').addEventListener('click', () => {
+    // alert('You clicked to write a new tweet!');
   });
   
 });
