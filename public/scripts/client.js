5388423d049cdea594f5ee855fb92bdbfb24f3b6
Alvin <alvin.tol@hotmail.com>
2022-03-30 04:52:25 +0000

    removes previous hardcoded placeholder tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,32 +2,33 @@ $(() => {
 
   const data = [
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
+      'user': {
+        'name': 'Newton',
+        'avatars': 'https://i.imgur.com/73hZDYK.png'
         ,
-        "handle": "@SirIstweetc"
+        'handle': '@SirIstweetc'
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      'content': {
+        'text': 'If I have seen further it is by standing on the shoulders of giants'
       },
-      "created_at": 1461116232227
+      'created_at': 1461116232227
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
+      'user': {
+        'name': 'Descartes',
+        'avatars': 'https://i.imgur.com/nlhLi3I.png',
+        'handle': '@rd'
       },
-      "content": {
-        "text": "Je pense , donc je suis"
+      'content': {
+        'text': 'Je pense , donc je suis'
       },
-      "created_at": 1461113959088
+      'created_at': 1461113959088
     }
-  ]
+  ];
 
+  //BUILDS MARKUP AND USING RELEVANT INFO FROM TWEET DATABASE 
   const createTweetElement = (tweet) => {
-    
+
     let $tweet = $(`<article class='tweet'>
     <div class='tweet-top'>
       <img src='${tweet.user.avatars}'></img>
@@ -37,29 +38,28 @@ $(() => {
       ${tweet.content.text}
     </p>            
     <div class='container tweet-bottom'>
-      <footer>${Date (tweet.created_at)}</footer>
+      <footer>${Date(tweet.created_at)}</footer>
       <div class='icons'>
-        <i class="icon fa-solid fa-flag"></i>
-        <i class="icon fa-solid fa-retweet"></i>
-        <i class="icon fa-solid fa-heart"></i>
+        <i class='icon fa-solid fa-flag'></i>
+        <i class='icon fa-solid fa-retweet'></i>
+        <i class='icon fa-solid fa-heart'></i>
       </div>
     </div>
     </article>`);
 
     return $tweet;
-  }
+  };
+
+  //GENERATES THE HTML MARKUP INTO INDEX.JS FILE 
   const renderTweets = (tweetsInData) => {
     const feed = document.querySelector('#tweet-feed');
+
+    //FUNCTION FOR LOOPING THROUGH ALL SEPERATE TWEETS IN DATABASE
     for (let singleTweet = 0; singleTweet < tweetsInData.length; tweet++) {
-
-      console.log(tweetsInData[singleTweet])
       const tweetArticle = createTweetElement(tweetsInData[singleTweet]);
-      console.log("TWEETARTICLE:",tweetArticle)
       return feed.prepend(tweetArticle[0]);
-     
     };
-
-  }
+  };
 
   renderTweets(data);
 });
