4c02f45920bdd5dbe2a9382b433a0101ccf09d13
Alvin <alvin.tol@hotmail.com>
2022-03-31 01:46:56 +0000

    adds timeago.js for date formatting

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,7 +1,8 @@
 $(() => {
 
-  let tweets = document.querySelectorAll('.tweet')
-  tweets.forEach(tweet => {
+  let tweets = document.querySelectorAll('.tweet');
+
+  tweets.forEach(function (tweet) {
     tweet.addEventListener('mouseover', function () {
       this.style.boxShadow = '2px 2px 2px 2px white';
       this.style.border = 'solid thick';
