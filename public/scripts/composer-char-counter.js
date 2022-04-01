8e7cba48a77f81c3e73be5a98ad2ac9d7aa09187
Alvin <alvin.tol@hotmail.com>
2022-04-01 00:22:17 +0000

    adds sass css files and fix longtweet wrod wrap issue

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,20 +12,21 @@ $(() => {
       counter.innerHTML = counterTotal;
 
     //turns red if counter is negative
-    counterTotal < 0 ?
+    counterTotal < 0 ? 
       counter.style.color = 'red' :
       counter.style.color = 'black';
 
     //displays error if character limit hit
-    textareaLength > 140 ? 
-    $('#full').slideDown('slow') : 
-    $('#full').slideUp('slow');
+    textareaLength > 140 ?
+      $('#full').slideDown('slow') :
+      $('#full').slideUp('slow');
+
 
     //display error html if textarea is empty
     if (textareaLength > 0) {
       $('#empty').slideUp('slow');
     };
-    
+
   });
 
 });
